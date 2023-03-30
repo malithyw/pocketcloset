b49f528edc8893522efea5aca590317ff799a1ea
Malithy <malithyw@gmail.com>
2023-03-30 11:19:44 -0500

    adding images

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -8,6 +8,11 @@ import Lookbooks from "./pages/Lookbooks";
 import Closet from "./pages/Closet";
 import Calendar from "./pages/Calendar";
 import Camera from "./pages/Camera";
+import cameraPNG from "./images/camera.png"
+import homePNG from "./images/house.png"
+import closetPNG from "./images/hanger.png"
+import calendarPNG from "./images/calendar.png"
+
 
 function App() {
   // const app = express();
@@ -22,20 +27,30 @@ function App() {
         <HashRouter>
           <Navbar bg="light" fixed="bottom">
             <Container>
-              <Navbar.Brand href="#">(home)</Navbar.Brand>
+              {/* <Navbar.Brand href="#">(home)</Navbar.Brand> */}
               <Nav className="me-auto">
-                {/* <Nav.Link href="/home">(home icon)</Nav.Link> */}
-                <Nav.Link href="#/calendar">(calendar)</Nav.Link>
-                <Nav.Link href="#/lookbooks">(lookbooks)</Nav.Link>
-                <Nav.Link href="#/closet">(closet)</Nav.Link>
-                <Nav.Link href="#/camera">(camera)</Nav.Link>
+                <Nav.Link href="#"> <img
+      src={require(homePNG)}
+      className='img-fluid shadow-4' height={height} width={width}
+      alt='not working'/></Nav.Link>
+                <Nav.Link href="#/calendar"><img
+      src={require(calendarPNG)}
+      className='img-fluid shadow-4' height={height} width={width}
+      alt='not working'/></Nav.Link>
+                <Nav.Link href="#/closet"><img
+      src={require(closetPNG)}
+      className='img-fluid shadow-4' height={height} width={width}
+      alt='not working'/></Nav.Link>
+                <Nav.Link href="#/camera"><img
+      src={require(cameraPNG)}
+      className='img-fluid shadow-4' height={height} width={width}
+      alt='not working'/></Nav.Link>
               </Nav>
             </Container>
           </Navbar>
           <Routes>
             <Route exact path="/" element={<Home />} />
             <Route exact path="/calendar" element={<Calendar />} />
-            <Route exact path="/lookbooks" element={<Lookbooks />} />
             <Route exact path="/closet" element={<Closet />} />
             <Route exact path="/camera" element={<Camera />} />
             <Route exact path="/home" element={<Home />} />
--- front-end/src/pages/Home.js
+++ front-end/src/pages/Home.js
@@ -2,12 +2,19 @@ import React from 'react';
 import { Link } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.css';
 import './Home.css';
+import useWindowDimensions from '../dimensions';'../dimensions'
 
 const Home = () =>  {
+    const { height, width } = useWindowDimensions();
+
     console.log("home")
     return (
         <div>
-            <p>home page!</p>
+            <img
+      src={require('../images/real_background.png')}
+      className='img-fluid shadow-4' height={height} width={width}
+      alt='not working'/>
+
         </div>
     );
 }
