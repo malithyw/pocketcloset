1fb63173621c7b1cfe91e64a11fb38c9459d8b08
Rik Ghosh <rikghosh487@gmail.com>
2023-03-30 02:39:33 -0500

    Calendar page updated

--- front-end/src/pages/Calendar.js
+++ front-end/src/pages/Calendar.js
@@ -1,87 +1,25 @@
 import React from "react";
-import FullCalendar from "@fullcalendar/react";
-import dayGridPlugin from "@fullcalendar/daygrid";
-import timeGridPlugin from "@fullcalendar/timegrid";
-import interactionPlugin from "@fullcalendar/interaction";
-import { INITIAL_EVENTS, createEventID } from "./InitialEvents";
-import "./Calendar.css";
+import Stack from "@mui/material/Stack";
+import CalendarComponent from "../components/CalendarComponent";
+import NewEventDialog from "../components/NewEventFormComponent";
+import UpcomingEvents from "../components/UpcomingEvents";
+import { EVENTS } from "../components/staticEvents";
 
-export default class Calendar extends React.Component {
-  state = {
-    currentEvents: [],
-  };
+const Calendar = () => {
+  let [currentDate, setCurrentDate] = React.useState("");
+  let [events, setEvents] = React.useState(EVENTS);
 
-  render() {
-    return (
-      <div className="calendar-housing">
-        <FullCalendar
-          plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
-          headerToolbar={{
-            left: "prev,next today",
-            center: "title",
-            right: "dayGridMonth,timeGridWeek",
-          }}
-          initialView="dayGridMonth"
-          editable={true}
-          selectable={true}
-          selectMirror={true}
-          dayMaxEvents={true}
-          initialEvents={INITIAL_EVENTS} // can use events setting to fetch from a feed
-          select={this.handleDateSelect}
-          eventContent={renderEventContent} // custom render function
-          eventClick={this.handleEventClick}
-          // eventSet={this.handleEvents} // called after events are initialized/added/changed/removed
-          /*
-            add these to update a remote database when these fire:
-
-            eventAdd={() => {}}
-            eventChange={() => {}}
-            eventRemove={() => {}}
-          */
-        />
-      </div>
-    );
-  }
-
-  handleDateSelect = (selectInfo) => {
-    let title = prompt("Please enter a new title for your event");
-    let calendarAPI = selectInfo.view.calendar;
-
-    calendarAPI.unselect();
-
-    if (title) {
-      calendarAPI.addEvent({
-        id: createEventID(),
-        title,
-        start: selectInfo.startStr,
-        end: selectInfo.endStr,
-        allDay: selectInfo.allDay,
-      });
-    }
-  };
-
-  handleEventClick = (clickInfo) => {
-    if (
-      confirm(
-        `Are you sure you want to delete the event "${clickInfo.event.title}"`
-      )
-    ) {
-      clickInfo.event.remove();
-    }
-  };
-
-  handleEvents = (events) => {
-    this.setState({
-      currentEvents: events,
-    });
-  };
-}
-
-const renderEventContent = (eventInfo) => {
   return (
-    <>
-      <b>{eventInfo.timeText}</b>
-      <i>{eventInfo.event.title}</i>
-    </>
+    <Stack
+      direction="column"
+      spacing={0}
+      sx={{ alignItems: "center", justifyContent: "center" }}
+    >
+      <CalendarComponent setCurrentDay={setCurrentDate} events={events} />
+      <NewEventDialog events={events} eventsModifier={setEvents} />
+      <UpcomingEvents eventMap={events} eventMapModifier={setEvents} />
+    </Stack>
   );
 };
+
+export default Calendar;
