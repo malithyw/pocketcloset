97dbe9cf7fe3a787cd69be233d1649237ee63281
Lauren Warhola <laurenwarhola@Laurens-MacBook-Pro-5.local>
2023-04-05 00:12:34 -0500

    adding to database

--- front-end/src/pages/Camera.js
+++ front-end/src/pages/Camera.js
@@ -9,6 +9,21 @@ import closetPNG from "../images/hanger.png"
 import calendarPNG from "../images/calendar.png"
 import settingsPNG from "../images/settings.png"
 
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDiomZpBaGnw99a60AA2u6rgA3wCmU_wXg",
+    authDomain: "pocketcloset-542e3.firebaseapp.com",
+    databaseURL: "https://pocketcloset-542e3-default-rtdb.firebaseio.com",
+    projectId: "pocketcloset-542e3",
+    storageBucket: "pocketcloset-542e3.appspot.com",
+    messagingSenderId: "730839004045",
+    appId: "1:730839004045:web:0d1b296e019d2660b7c3c4",
+    measurementId: "G-N05QV7RL04"
+  };
+
+const app = initializeApp(firebaseConfig);
+
 const Camera = (props) => {
     const height = 70;
     const width = 70;
@@ -16,6 +31,8 @@ const Camera = (props) => {
     const [contin, setContin] = React.useState(false);
     const [tags, setTags] = React.useState([]);
     const [newTag, setNewTag] = React.useState("");
+    const [name, setName] = React.useState("");
+    const user = props.user;
 
     const handleCameraComponentImage = (data) => {
         setPicture(data[0]);
@@ -41,22 +58,49 @@ const Camera = (props) => {
     function updateInput(event) {
         if (event.target.id === "tagInput") {
             setNewTag(event.target.value);
+        } else if (event.target.id === "nameInput") { 
+            setName(event.target.value);
         }
+    }
+
+    function save() { 
+        let piece = {
+                "image": picture,
+                "name": name,
+                "tags": tags
+        };
+        //add to closet, get user ID and add info below
+        fetch(`${firebaseConfig.databaseURL + "/users/" + user.uid}/closet/clothes/.json`, { method: 'POST', body: JSON.stringify(piece) })
+                .then((res) => {
+                    if (res.status !== 200) {
+                        console.log("piece not stored");
+                    } else {
+                        console.log("piece stored");
+                        alert("Piece has been added to closet");
+                    }
+                }).catch(error => alert(error))
+        setTags([]);
+        setName("");
+        setNewTag("");
+        setPicture("");
+        goBack();//because it restarts camera
     }
 
     return (
         <div>
-            <p>Add to Your Virtual Closet</p>
+            <a>Add to Your Virtual Closet</a>
             {contin ?
                 <div>
                     <button onClick={goBack}>{"<"}</button>
                     <img src={picture} />
+                    <p>Name the Piece</p>
+                    <input id="nameInput" onChange={updateInput} />
                     <p>Add Tags for this Piece</p>
                     <input id="tagInput" onChange={updateInput} />
                     <button onClick={addTag}>Add</button>
                     {tags.map(tag => <li>{tag}</li>)}
                     {/* //create tag objects later that have x for deletion */}
-                    <button>Save</button>
+                    <button onClick={save}>Save</button>
                     {/* //needs to go to closet to view piece? or go back to camera--needs to check if any tags were created*/}
                 </div>
                 :
